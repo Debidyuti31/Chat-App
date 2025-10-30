894f890ba3423c1083c4d2a02578e78636636f60
Debidyuti Kar <122683342+Debidyuti31@users.noreply.github.com>
2025-10-30 20:28:53 +0530

    Add files via upload

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCj67fu0fienJX3coBWCM_NTk6qlvBJLHc",
+  authDomain: "chat-app-gs-1bbed.firebaseapp.com",
+  projectId: "chat-app-gs-1bbed",
+  storageBucket: "chat-app-gs-1bbed.firebasestorage.app",
+  messagingSenderId: "155526734165",
+  appId: "1:155526734165:web:d493d00438d5651f8dcfdc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
